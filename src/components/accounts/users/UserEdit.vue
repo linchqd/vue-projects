7b72e9878eb932702eee5437f9e16cfc19dc13d0
linchqd <linchqd@163.com>
2019-12-02 17:36:47 +0800

    deploy for docker

--- src/components/accounts/users/UserEdit.vue
+++ src/components/accounts/users/UserEdit.vue
@@ -6,7 +6,7 @@
         <span>返回</span>
       </div>
       <span class="content-title-info">角色信息</span>
-      <el-button type="primary" plain size="mini">编辑</el-button>
+      <el-button type="primary" plain size="mini" @click.native="userUpdateFromVisible = true">编辑</el-button>
     </div>
     <div class="content-content">
       <table class="table">
@@ -150,6 +150,26 @@
         </el-tabs>
       </div>
     </div>
+    <el-dialog :title="userUpdateFormTitle" :visible.sync="userUpdateFromVisible" :close-on-click-modal="false">
+        <el-form ref="userUpdateFrom" :model="userUpdateFromModel" :rules="rules" label-width="80px">
+          <el-form-item prop="name" label="登录名">
+              <el-input placeholder="username" v-model="userUpdateFromModel.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item prop="cname" label="姓名">
+              <el-input placeholder="cname" v-model="userUpdateFromModel.cname" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item prop="email" label="邮箱">
+              <el-input placeholder="email" v-model="userUpdateFromModel.email" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item prop="phone_number" label="手机号码" required>
+              <el-input placeholder="phone number" v-model="userUpdateFromModel.phone_number" auto-complete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="danger" size="small" plain @click.native="userUpdateFromVisible = false">取 消</el-button>
+          <el-button type="primary" size="small" :loading="userUpdateFromLoading" plain @click.native="userUpdateFromSubmit">确 定</el-button>
+        </div>
+      </el-dialog>
   </div>
 </template>
 <script>
@@ -157,21 +177,105 @@ import dateFormat from '../../../libs/formatDatetime.js'
 export default {
   name: 'userEdit',
   data () {
+    let checkPhone = (rule, value, callback) => {
+      if (value === '') {
+        return callback(new Error('手机号不能为空'))
+      }
+      setTimeout(() => {
+        let mobile = /^1[34578]\d{9}$/
+        if (!mobile.test(value)) {
+          callback(new Error('手机号不正确'))
+        } else {
+          callback()
+        }
+      }, 1000)
+    }
+    let validatePwd = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.user_add_form.password2 !== '') {
+          this.$refs.user_add_form.validateField('password2')
+        }
+        callback()
+      }
+    }
+    let validatePwd2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.user_add_form.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
       userObj: {},
-      activeName: 'hasGroups'
+      activeName: 'hasGroups',
+      userUpdateFormTitle: '更新用户',
+      userUpdateFromVisible: false,
+      userUpdateFromLoading: false,
+      rules: {
+        name: [
+          { required: true, message: '请输入登录名', trigger: 'blur' }
+        ],
+        cname: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
+        ],
+        phone_number: [
+          { validator: checkPhone, trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
+        ],
+        password: [
+          { validator: validatePwd, trigger: 'blur' }
+        ],
+        password2: [
+          { validator: validatePwd2, trigger: 'blur' }
+        ]
+      },
+      userUpdateFromModel: {
+        id: Number,
+        name: '',
+        cname: '',
+        email: '',
+        phone_number: ''
+      }
     }
   },
   methods: {
     get_users (username) {
       this.$http.get(`/accounts/users/?name=${username}`).then(response => {
         this.userObj = response.res
+        for (let item in this.userUpdateFromModel) {
+          if (this.userObj.hasOwnProperty(item)) {
+            this.userUpdateFromModel[item] = this.userObj[item]
+          }
+        }
       }, error => {
         this.$custom_message('error', error.res)
       })
     },
     handleClick (tab, event) {
       console.log(tab, event)
+    },
+    userUpdateFromSubmit () {
+      this.$refs.userUpdateFrom.validate((pass) => {
+        if (pass) {
+          this.userUpdateFromLoading = true
+          this.$http.put('/accounts/users/', this.userUpdateFromModel).then(response => {
+            this.$custom_message('success', response.res)
+            this.userUpdateFromVisible = false
+            this.get_users(this.userObj.name)
+          }, error => {
+            this.$custom_message('error', error.res)
+          }).finally(() => {
+            this.userUpdateFromLoading = false
+          })
+        }
+      })
     }
   },
   created () {
